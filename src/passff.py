383fa3f49e16df6157d3b8c72259574d2400e3f7
Jason Taylor <jason@infinitebubble.com>
2024-05-09 21:43:33 -0400

    Add setPassGpgOpts function

--- src/passff.py
+++ src/passff.py
@@ -123,6 +123,23 @@ def _print_tree(tree, seperators=None):
 
     return tree_out
 
+def setPassGpgOpts(env, opts_dict):
+    """ Add arguments to PASSWORD_STORE_GPG_OPTS. """
+    opts = env.get('PASSWORD_STORE_GPG_OPTS', '')
+    for opt, value in opts_dict.items():
+        re_opt = new_opt = opt
+        if value is not None:
+            re_opt = rf"{opt}(?:=|\s+)\S*"
+            new_opt = (
+                f"{opt}={shlex.quote(value)}"
+                if opt.startswith("--") else
+                f"{opt} {shlex.quote(value)}"
+            )
+        # If the user's environment sets this opt, remove it.
+        opts = re.sub(re_opt, '', opts)
+        opts = f"{new_opt} {opts}"
+    env['PASSWORD_STORE_GPG_OPTS'] = opts.strip()
+
 if __name__ == "__main__":
     # Read message from standard input
     receivedMessage = getMessage()
